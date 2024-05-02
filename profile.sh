9e921b0ff65ec01b9286289158f2920a8a9242fe
Taha Azzaoui <taha@azzaoui.org>
2024-05-02 14:09:26 -0400

    update script

--- profile.sh
+++ profile.sh
@@ -10,12 +10,25 @@ DATA=$ROOT/data
 MAT_NAME=$1
 MAT_PATH=$DATA/$MAT_NAME/$MAT_NAME
 RUNS=5
+NCOUNTERS=4
+
+# Group events by counter and run perf
+function run_once() {
+    rm -rf out && mkdir -p out
+    for ((i = 0; i < ${#EVENTS[@]}; i += $NCOUNTERS))
+    do
+        nevents=("${EVENTS[@]:i:$NCOUNTERS}")
+        nevents=${nevents[*]}
+        e_arg=${nevents// /,}
+        sudo perf stat -j -o out/$i.json -e $e_arg $EXE --matrix $1 2>&1 > /dev/null
+    done
+    cat out/*.json > $2
+}
 
 # install deps
 sudo apt update
 sudo apt install -y flex bison libelf-dev libtraceevent-dev
 sudo apt install -y pkg-config cmake python3-dev python3-numpy python3-scipy
-sudo apt install -y cargo rustc
 
 # build perf
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
@@ -38,17 +51,13 @@ cd $ROOT
 git clone https://github.com/uml-hpc/adsp.git
 cd adsp && cd cpuid && make
 ADSP=$ROOT/adsp/adsp
+EVENTS=(`sudo $ADSP perf_list --perf-path /usr/local/bin`)
 
 cd $ROOT
 
 # build mat error
 git clone https://github.com/uml-hpc/materror.git
 MAT_ERR=$ROOT/materror/materror
-
-# build perf-rs
-git clone https://github.com/uml-hpc/perf-rs.git
-cd perf-rs && cargo build --release
-PERF_RS=$ROOT/perf-rs/target/release/perf-rs
 
 cd $ROOT
 
@@ -76,5 +85,8 @@ done
 for FILE in $DATA/$MAT_NAME/*; do
     $ADSP mtx2crs --matrix $FILE --out $FILE.crs
     echo "Profiling $FILE"
-    sudo $PERF_RS  --runs $RUNS --out $FILE.json --bin $EXE -a --matrix $FILE
+    for RUN in `seq $RUNS`; do
+        echo "RUN $RUN"
+        run_once $FILE.crs ${FILE}_${RUN}.json
+    done
 done
